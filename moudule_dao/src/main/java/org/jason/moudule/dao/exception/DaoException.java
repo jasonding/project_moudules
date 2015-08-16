51cc6788fe95250d6bfd65e4c4561f6deb977de9
jason <jason@jasondeMacBook-Pro.local>
2015-08-16 21:32:32 +0800

    Initial commit

---
+++ moudule_dao/src/main/java/org/jason/moudule/dao/exception/DaoException.java
@@ -0,0 +1,26 @@
+package org.jason.moudule.dao.exception;
+
+/**
+ * Created by jason on 14-10-12.
+ */
+public class DaoException extends RuntimeException {
+    public DaoException() {
+        super();
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+
+    protected DaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ moudule_dao/src/main/java/org/jason/moudule/dao/hibernate/base/DaoBaseImpl.java
@@ -0,0 +1,164 @@
+package org.jason.moudule.dao.hibernate.base;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.jason.moudule.dao.exception.DaoException;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.*;
+
+/**
+ * Created by jason on 14-9-27.
+ */
+public abstract class DaoBaseImpl<T> implements DaoBase<T> {
+
+
+    private Class<T> clazz = null;
+
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public DaoBaseImpl() {
+        ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
+        clazz = (Class<T>) type.getActualTypeArguments()[0];
+    }
+
+    @Override
+    public void delete(T entity) {
+        try {
+            this.getSession().delete(entity);
+        } catch (Exception e) {
+            throw new DaoException("删除实体失败",e);
+        }
+    }
+
+    @Override
+    public T getById(Serializable id) {
+        Object object = this.getSession().get(clazz, id);
+        return (T) object;
+    }
+
+    @Override
+    public List<T> findByIds(final Collection<Serializable> ids) {
+        StringBuilder hql = new StringBuilder("FROM");
+        hql.append(clazz.getSimpleName()).append("WHERE 1=1");
+        List<Object> list = new ArrayList<Object>();
+        if (ids.size() == 1) {
+            hql.append(" AND id = ? ");
+            list.add(ids.iterator().next());
+        } else {
+            hql.append(" AND id IN ( ");
+            for (Serializable id : ids) {
+                hql.append(" ? ,");
+                list.add(id);
+            }
+            hql = hql.deleteCharAt(hql.lastIndexOf(","));
+            hql.append(")");
+        }
+        Query query = this.getSession().createQuery(hql.toString());
+        for (int i = 0; i < ids.size(); i++) {
+            query.setParameter(i, list.get(i));
+        }
+        return query.list();
+    }
+
+    @Override
+    public List<T> findAll() {
+        return this.getSession().createQuery("FROM " + clazz.getSimpleName()).list();
+    }
+
+    @Override
+    public void save(T entity) {
+        try {
+            this.getSession().save(entity);
+        } catch (Exception e) {
+            throw new DaoException("保存实体失败",e);
+        }
+    }
+
+    @Override
+    public void update(T entity) {
+        try {
+            this.getSession().update(entity);
+        } catch (Exception e) {
+            throw new DaoException("更新实体失败",e);
+        }
+    }
+
+    @Override
+    public Integer getCount(Map<String, String> params) {
+        StringBuilder hql = new StringBuilder(" SELECT COUNT(*) FROM ");
+        hql.append(clazz.getSimpleName()).append(" WHERE 1=1 ");
+        List<Object> list = buildQueryCondition(params,hql);
+        Query query = this.getSession().createQuery(hql.toString());
+        for(int i=0; i<list.size(); i++) {
+            query.setParameter(i, list.get(i));
+        }
+        return ((Number) query.uniqueResult()).intValue();
+    }
+
+    @Override
+    public List<T> getPageList(Map<String, String> params, Integer firstRow,Integer maxRow) {
+        return getPageList(params, null,firstRow, maxRow);
+    }
+
+    @Override
+    public List<T> getPageList(Map<String, String> params, LinkedHashMap<String, String> orderMap, Integer firstRow, Integer maxRow) {
+        StringBuilder hql = new StringBuilder(" FROM ");
+        hql.append(clazz.getSimpleName()).append(" WHERE 1=1 ");
+        //构建查询条件
+        List<Object> list = buildQueryCondition(params,hql);
+        Query query = this.getSession().createQuery(hql.toString());
+        for(int i=0; i<list.size(); i++) {
+            query.setParameter(i, list.get(i));
+        }
+        //构建排序结果
+        hql.append(buildOrderCondition(orderMap));
+        return query.setFirstResult(firstRow).setMaxResults(maxRow).list();
+    }
+
+    @Override
+    public void merge(T entity) {
+        try {
+            this.getSession().merge(entity);
+        } catch (Exception e) {
+            throw new DaoException("merge实体失败",e);
+        }
+    }
+
+    protected static String buildOrderCondition(LinkedHashMap<String, String> orderMap) {
+        if (orderMap == null || orderMap.isEmpty())
+            return "";
+        StringBuilder orderStr = new StringBuilder(" ORDER BY ");
+        for (Map.Entry<String, String> entry : orderMap.entrySet()) {
+            orderStr.append(entry.getKey()).append(" ")
+                    .append(entry.getValue()).append(",");
+        }
+        orderStr.deleteCharAt(orderStr.length() - 1);
+        return orderStr.toString();
+    }
+
+    /**
+     * 得到当前线程session
+     * @return
+     */
+    protected final Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    /**
+     * 构建条件查询语句(由各个子类去实现)
+     *
+     * @param params
+     * @return
+     */
+    protected List<Object> buildQueryCondition(Map<String,String> params,StringBuilder hql) {
+        return new ArrayList<Object>();
+    }
+
+}
